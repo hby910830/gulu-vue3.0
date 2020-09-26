b1e50d374ac0218d5df40cc138b689cdf16147ec
hanbaoyi <604594231@qq.com>
2020-09-27 00:21:05 +0800

    openDialog 一句话打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -16,11 +16,15 @@
       <p>123</p>
     </template>
   </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">toggle</Button>
+  <Dialog></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/Button.vue'
+import {openDialog} from '../lib/openDialog'
 import {ref} from 'vue'
 
 export default {
@@ -37,8 +41,22 @@ export default {
     const f2 = () => {
 
     }
+    const showDialog = () => {
+      openDialog({
+        title: '标题',
+        content: '你好',
+        closeOnClickOverlay: false,
+        ok(){
+          console.log('ok')
+        },
+        cancel(){
+          console.log('cancel')
+        }
+      })
+    }
     return {
       visible,
+      showDialog,
       toggle,
       f1,
       f2
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -47,7 +47,7 @@ export default {
       context.emit('update:visible', false)
     }
     const cancel = () => {
-      context.emit('cancel')
+      props.cancel && props.cancel() && close()
       close()
     }
     const ok = () => {
